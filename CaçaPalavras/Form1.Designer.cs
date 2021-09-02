025250c5f7e4908ac552bb1071bd205c57e8fe17
GRCcoelho <coelhogabriel2411@gmail.com>
2021-09-01 21:59:14 -0300

    Layout

--- CaçaPalavras/Form1.Designer.cs
+++ CaçaPalavras/Form1.Designer.cs
@@ -29,13 +29,243 @@ namespace CaçaPalavras
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.btn00 = new System.Windows.Forms.Button();
+            this.btn01 = new System.Windows.Forms.Button();
+            this.btn03 = new System.Windows.Forms.Button();
+            this.btn10 = new System.Windows.Forms.Button();
+            this.btn11 = new System.Windows.Forms.Button();
+            this.btn12 = new System.Windows.Forms.Button();
+            this.btn20 = new System.Windows.Forms.Button();
+            this.btn21 = new System.Windows.Forms.Button();
+            this.btn22 = new System.Windows.Forms.Button();
+            this.lblTitulo = new System.Windows.Forms.Label();
+            this.btnSair = new System.Windows.Forms.Button();
+            this.btnNovoTabuleiro = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.lblInsiraPalavra = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.colJogador = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colPontuacao = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btn00
+            // 
+            this.btn00.Location = new System.Drawing.Point(24, 55);
+            this.btn00.Name = "btn00";
+            this.btn00.Size = new System.Drawing.Size(100, 100);
+            this.btn00.TabIndex = 0;
+            this.btn00.UseVisualStyleBackColor = true;
+            // 
+            // btn01
+            // 
+            this.btn01.Location = new System.Drawing.Point(130, 55);
+            this.btn01.Name = "btn01";
+            this.btn01.Size = new System.Drawing.Size(100, 100);
+            this.btn01.TabIndex = 1;
+            this.btn01.UseVisualStyleBackColor = true;
+            // 
+            // btn03
+            // 
+            this.btn03.Location = new System.Drawing.Point(236, 55);
+            this.btn03.Name = "btn03";
+            this.btn03.Size = new System.Drawing.Size(100, 100);
+            this.btn03.TabIndex = 2;
+            this.btn03.UseVisualStyleBackColor = true;
+            // 
+            // btn10
+            // 
+            this.btn10.Location = new System.Drawing.Point(24, 161);
+            this.btn10.Name = "btn10";
+            this.btn10.Size = new System.Drawing.Size(100, 100);
+            this.btn10.TabIndex = 3;
+            this.btn10.UseVisualStyleBackColor = true;
+            // 
+            // btn11
+            // 
+            this.btn11.Location = new System.Drawing.Point(130, 161);
+            this.btn11.Name = "btn11";
+            this.btn11.Size = new System.Drawing.Size(100, 100);
+            this.btn11.TabIndex = 4;
+            this.btn11.UseVisualStyleBackColor = true;
+            // 
+            // btn12
+            // 
+            this.btn12.Location = new System.Drawing.Point(236, 161);
+            this.btn12.Name = "btn12";
+            this.btn12.Size = new System.Drawing.Size(100, 100);
+            this.btn12.TabIndex = 5;
+            this.btn12.UseVisualStyleBackColor = true;
+            // 
+            // btn20
+            // 
+            this.btn20.Location = new System.Drawing.Point(24, 267);
+            this.btn20.Name = "btn20";
+            this.btn20.Size = new System.Drawing.Size(100, 100);
+            this.btn20.TabIndex = 6;
+            this.btn20.UseVisualStyleBackColor = true;
+            // 
+            // btn21
+            // 
+            this.btn21.Location = new System.Drawing.Point(130, 269);
+            this.btn21.Name = "btn21";
+            this.btn21.Size = new System.Drawing.Size(100, 97);
+            this.btn21.TabIndex = 7;
+            this.btn21.UseVisualStyleBackColor = true;
+            // 
+            // btn22
+            // 
+            this.btn22.Location = new System.Drawing.Point(236, 269);
+            this.btn22.Name = "btn22";
+            this.btn22.Size = new System.Drawing.Size(100, 100);
+            this.btn22.TabIndex = 8;
+            this.btn22.UseVisualStyleBackColor = true;
+            // 
+            // lblTitulo
+            // 
+            this.lblTitulo.AutoSize = true;
+            this.lblTitulo.Font = new System.Drawing.Font("Trebuchet MS", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitulo.Location = new System.Drawing.Point(16, 9);
+            this.lblTitulo.Name = "lblTitulo";
+            this.lblTitulo.Size = new System.Drawing.Size(256, 43);
+            this.lblTitulo.TabIndex = 9;
+            this.lblTitulo.Text = "CAÇA-PALAVRAS";
+            // 
+            // btnSair
+            // 
+            this.btnSair.BackColor = System.Drawing.Color.Red;
+            this.btnSair.Location = new System.Drawing.Point(676, 401);
+            this.btnSair.Name = "btnSair";
+            this.btnSair.Size = new System.Drawing.Size(112, 37);
+            this.btnSair.TabIndex = 10;
+            this.btnSair.Text = "SAIR";
+            this.btnSair.UseVisualStyleBackColor = false;
+            // 
+            // btnNovoTabuleiro
+            // 
+            this.btnNovoTabuleiro.BackColor = System.Drawing.Color.DeepSkyBlue;
+            this.btnNovoTabuleiro.Location = new System.Drawing.Point(466, 401);
+            this.btnNovoTabuleiro.Name = "btnNovoTabuleiro";
+            this.btnNovoTabuleiro.Size = new System.Drawing.Size(112, 37);
+            this.btnNovoTabuleiro.TabIndex = 11;
+            this.btnNovoTabuleiro.Text = "NOVO TABULEIRO";
+            this.btnNovoTabuleiro.UseVisualStyleBackColor = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(24, 395);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(252, 38);
+            this.textBox1.TabIndex = 12;
+            // 
+            // lblInsiraPalavra
+            // 
+            this.lblInsiraPalavra.AutoSize = true;
+            this.lblInsiraPalavra.Font = new System.Drawing.Font("Trebuchet MS", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblInsiraPalavra.Location = new System.Drawing.Point(19, 370);
+            this.lblInsiraPalavra.Name = "lblInsiraPalavra";
+            this.lblInsiraPalavra.Size = new System.Drawing.Size(204, 27);
+            this.lblInsiraPalavra.TabIndex = 13;
+            this.lblInsiraPalavra.Text = "Insira uma palavra:";
+            // 
+            // dataGridView1
+            // 
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridView1.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.colJogador,
+            this.colPontuacao});
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dataGridView1.DefaultCellStyle = dataGridViewCellStyle2;
+            this.dataGridView1.Location = new System.Drawing.Point(466, 56);
+            this.dataGridView1.Name = "dataGridView1";
+            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle3.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle3.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle3.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle3.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridView1.RowHeadersDefaultCellStyle = dataGridViewCellStyle3;
+            this.dataGridView1.Size = new System.Drawing.Size(312, 311);
+            this.dataGridView1.TabIndex = 14;
+            // 
+            // colJogador
+            // 
+            this.colJogador.HeaderText = "Jogador";
+            this.colJogador.Name = "colJogador";
+            // 
+            // colPontuacao
+            // 
+            this.colPontuacao.HeaderText = "Pontuação";
+            this.colPontuacao.Name = "colPontuacao";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LimeGreen;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.lblInsiraPalavra);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnNovoTabuleiro);
+            this.Controls.Add(this.btnSair);
+            this.Controls.Add(this.lblTitulo);
+            this.Controls.Add(this.btn22);
+            this.Controls.Add(this.btn21);
+            this.Controls.Add(this.btn20);
+            this.Controls.Add(this.btn12);
+            this.Controls.Add(this.btn11);
+            this.Controls.Add(this.btn10);
+            this.Controls.Add(this.btn03);
+            this.Controls.Add(this.btn01);
+            this.Controls.Add(this.btn00);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btn00;
+        private System.Windows.Forms.Button btn01;
+        private System.Windows.Forms.Button btn03;
+        private System.Windows.Forms.Button btn10;
+        private System.Windows.Forms.Button btn11;
+        private System.Windows.Forms.Button btn12;
+        private System.Windows.Forms.Button btn20;
+        private System.Windows.Forms.Button btn21;
+        private System.Windows.Forms.Button btn22;
+        private System.Windows.Forms.Label lblTitulo;
+        private System.Windows.Forms.Button btnSair;
+        private System.Windows.Forms.Button btnNovoTabuleiro;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label lblInsiraPalavra;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colJogador;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colPontuacao;
     }
 }
 
